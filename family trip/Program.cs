2a57e0bfaf94ef4806601a356042935d2300b21e
user <user@DESKTOP-Q7SRQOE>
2023-11-11 17:54:38 +0200

    Add project files.

---
+++ family trip/Program.cs
@@ -0,0 +1,24 @@
+﻿double budgetTheyHave = double.Parse(Console.ReadLine());   
+int sleepNights  = int.Parse(Console.ReadLine());
+double pricePerNight  = double.Parse(Console.ReadLine());
+int percentExtra = int.Parse(Console.ReadLine());
+
+
+double afterExtra = budgetTheyHave * percentExtra / 100;
+
+if (sleepNights > 7)
+{
+    pricePerNight -= pricePerNight * 0.05;
+    
+}
+ afterExtra += pricePerNight * sleepNights;
+if (budgetTheyHave >= afterExtra)
+{
+    Console.WriteLine($"Ivanovi will be left with {budgetTheyHave - afterExtra:F2} leva after vacation.");
+}
+
+else
+{
+    
+    Console.WriteLine($"{afterExtra - budgetTheyHave:F2} leva needed.");
+}
---
+++ official exam prepp market for fruits/Program.cs
@@ -0,0 +1,17 @@
+﻿double strawberyPriceInLev = double.Parse(Console.ReadLine());  
+double bananasInKg = double.Parse(Console.ReadLine());  
+double orangeInKg = double.Parse(Console.ReadLine());
+double rassberryInKg  = double.Parse(Console.ReadLine());
+double strawberryInKg = double.Parse(Console.ReadLine());
+
+double raspberryPrice = strawberyPriceInLev * 0.5;
+double orangePrise = raspberryPrice - (0.4 * raspberryPrice);
+double bananaPrise = raspberryPrice - (0.8 * raspberryPrice);
+double sumOfRasberry = rassberryInKg * raspberryPrice;
+double sumOfOrange = orangeInKg * orangePrise;
+double sumOfBanana = bananasInKg * bananaPrise;
+double sumOfStrawbery = strawberryInKg * strawberyPriceInLev;
+
+double neededMoney = sumOfBanana + sumOfStrawbery + sumOfRasberry + sumOfOrange;
+
+Console.WriteLine($"{neededMoney:F2}");
